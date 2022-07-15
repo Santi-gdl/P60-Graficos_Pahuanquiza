7fa661c7300c65c19520e1958f238b03c9227ba3
Gadiel Santiago <106574022+Santi-gdl@users.noreply.github.com>
2022-07-14 23:53:43 -0500

    Commit Final

---
+++ principal.cpp
@@ -0,0 +1,196 @@
+#include "principal.h"
+#include "ui_principal.h"
+
+
+Principal::Principal(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::Principal)
+{
+    ui->setupUi(this);
+    lienzo = QPixmap(500,500);
+    this->dibujar();
+
+    ui->outCuadro->setPixmap(lienzo);
+
+}
+
+Principal::~Principal()
+{
+    delete ui;
+}
+
+void Principal::paintEvent(QPaintEvent *event)
+{
+  ui->outCuadro->setPixmap(lienzo);
+
+}
+
+void Principal::dibujar()
+{
+    lienzo.fill(Qt::white);
+
+    QPainter painter(&lienzo);
+
+    int x = 0;
+    int y = 0;
+
+    // Crear un pincel para los bordes
+    QPen pincel;
+    pincel.setWidth(5);
+    pincel.setColor(Qt::blue);
+    pincel.setJoinStyle(Qt::MiterJoin);
+
+    // Establecer el pincel al "pintor"
+    painter.setPen(pincel);
+
+    //Obtener datos para la primera barra
+    int nota1 =ui->inNota1->value();
+    int altoN1 = this->getAlto(nota1);
+    int incYN1 = this->incY(altoN1);
+
+    // Dibujar primera barra
+    painter.drawRect(x+75, y+50+incYN1,100,altoN1);
+
+    // Crear un objeto color para el relleno
+    QColor colorRelleno(251, 234, 61);
+    // Crear otro objeto color para el borde
+    QColor colorBorde(129,117,7);
+
+    // Cambiar el color del pincel
+    pincel.setColor(colorBorde);
+
+    // Volver a establecer el pincel al "pintor"
+    painter.setPen(pincel);
+
+    // Establecer el color al brush (relleno)
+    painter.setBrush(colorRelleno);
+
+
+    //TRABAJO
+
+    //Obtener datos para la SEGUNDA barra
+    int nota2 =ui->inNota2->value();
+    int altoN2 = this->getAlto(nota2);
+    int incYN2 = this->incY(altoN2);
+
+    // Dibujar segunda barra
+    painter.drawRect(x+195, y+50+incYN2, 100, altoN2);
+
+    // Creando los colores de la tercera barra
+    QColor cRellenoBarra3(97, 141, 141);
+    QColor cBordeBarra3(25, 9, 41);
+
+    // Estableciendo colores al puncel y al painter
+    pincel.setColor(cBordeBarra3);
+    painter.setPen(pincel);
+    painter.setBrush(cRellenoBarra3);
+
+
+    //Obtener datos para la TERCERA barra
+    int nota3 =ui->inNota3->value();
+    int altoN3 = this->getAlto(nota3);
+    int incYN3 = this->incY(altoN3);
+
+
+
+
+
+    //Dibujar tercera barra
+    painter.drawRect(x+315,y+50+incYN3,100,altoN3);
+
+    string str = promedio();
+    QString qstr = QString::fromStdString(str);
+    ui->lineEdit->setText(qstr);
+
+    //Dibujar el promedio
+    painter.drawRect(x, 450 -((altoN1+altoN2+altoN3)/3), 500, 0.5);
+
+
+    //Dibujar el plano cartesiano X
+    painter.drawLine(x+25,y+450, x+475, y+450);
+
+    //Dibujar el plano cartesiano Y
+    painter.drawLine(x+50,y+50, x+50,y+475);
+
+    //Dibujar lineas
+    painter.drawLine(x+50,y+400, x+50,y+400);
+
+
+    // Dibujar nombres
+    painter.drawText(x+115,y+470, "N1");
+    painter.drawText(x+235,y+470, "N2");
+    painter.drawText(x+355,y+470, "N3");
+
+    int cont = 550;
+    int as = 0;
+    for (int i=0; i<=5; i++){
+        cont = cont-83;
+        qDebug()<<as<<""<<cont;
+        painter.drawText(15, cont, QString::number(as));
+        as = as+(5*4);
+    }
+
+}
+
+
+int Principal::getAlto(int valor)
+{
+    return 4 * valor;
+}
+
+int Principal::incY(int alto)
+{
+    return 400-alto;
+}
+
+string Principal::promedio()
+{
+    int nota1= ui->inNota1->value();
+    int nota2= ui->inNota2->value();
+    int nota3= ui->inNota3->value();
+
+    int promedio;
+    promedio= (nota1+nota2+nota3) /3;
+    string final="Promedio: " + to_string(promedio);
+    return final;
+}
+
+void Principal::on_actionGuardar_triggered()
+{
+    QString nombreArchivo = QFileDialog::getSaveFileName(
+                this,"Guardar imagen",QString(),"Imagenes (*.png)");
+    if (!nombreArchivo.isEmpty()){
+        if (lienzo.save(nombreArchivo))
+            QMessageBox::information(this,"Guardar imagen","Archivo guarado en: " + nombreArchivo);
+        else
+            QMessageBox::warning(this,"Guardar imagen","No se pudo guardar el archivo");
+    }
+}
+
+void Principal::on_pushButton_clicked(bool checked)
+{
+
+}
+
+void Principal::on_inNota1_valueChanged(int arg1)
+{
+    dibujar();
+}
+
+void Principal::on_inNota2_valueChanged(int arg1)
+{
+    dibujar();
+}
+
+void Principal::on_inNota3_valueChanged(int arg1)
+{    string str = promedio();
+     QString qstr = QString::fromStdString(str);
+     ui->lineEdit->setText(qstr);
+    dibujar();
+}
+
+void Principal::on_lineEdit_textEdited(const QString &arg1)
+{
+
+}
+
---
+++ principal.h
@@ -0,0 +1,52 @@
+#ifndef PRINCIPAL_H
+#define PRINCIPAL_H
+
+#include <QMainWindow>
+#include <QPainter>
+#include <QFileDialog>
+#include <QMessageBox>
+#include <QMainWindow>
+#include <string>
+#include <QDebug>
+
+using namespace std;
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class Principal; }
+QT_END_NAMESPACE
+
+class Principal : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    Principal(QWidget *parent = nullptr);
+    ~Principal();
+
+protected:
+    void paintEvent(QPaintEvent *event) override;
+
+private slots:
+    void on_actionGuardar_triggered();
+
+    void on_pushButton_clicked(bool checked);
+
+    void on_inNota1_valueChanged(int arg1);
+
+    void on_inNota2_valueChanged(int arg1);
+
+    void on_inNota3_valueChanged(int arg1);
+
+    void on_lineEdit_textEdited(const QString &arg1);
+
+private:
+    Ui::Principal *ui;
+    QPixmap lienzo;
+
+    void dibujar();
+    int getAlto (int valor);
+    int incY(int alto);
+
+    string promedio();
+};
+#endif // PRINCIPAL_H
